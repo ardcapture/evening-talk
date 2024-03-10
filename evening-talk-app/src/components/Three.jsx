030c1e04221eb5cbdb75ce87a716ce872a694873
Adrian Dawson <ardcapture@gmail.com>
2024-03-10 23:04:42 +0000

    first setup of three.js

---
+++ evening-talk-app/src/components/Three.jsx
@@ -0,0 +1,93 @@
+import { useEffect, useRef} from 'react';
+import * as THREE from 'three';
+
+
+
+
+const Three = () => {
+    const sceneRef = useRef(null);
+
+    const getBox = (w, h, d) => {
+        const geometry = new THREE.BoxGeometry(w,d,h);
+        const material = new THREE.MeshBasicMaterial({
+            color: 0x00ff00
+        });
+        const mesh = new THREE.Mesh(
+            geometry, material
+        );
+
+        return mesh;
+    }
+
+    const getPlane = (size) => {
+        const geometry = new THREE.PlaneGeometry(size, size);
+        const material = new THREE.MeshBasicMaterial({
+            color: 0xff0000,
+            side: THREE.DoubleSide
+        });
+        const mesh = new THREE.Mesh(
+            geometry, material
+        );
+
+        return mesh;
+    }
+
+    const init = () => {
+        const scene = new THREE.Scene();
+        
+        const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+        camera.position.x = 1
+        camera.position.y = 2
+        camera.position.z = 5
+        camera.lookAt(new THREE.Vector3(0,0,0))
+
+        const renderer = new THREE.WebGLRenderer();
+
+        renderer.setSize(window.innerWidth, window.innerHeight);
+        sceneRef.current.appendChild(renderer.domElement);
+
+        const box = getBox(1, 1, 1);
+        box.position.y = box.geometry.parameters.height/2;
+
+        const plane = getPlane(4);
+        plane.rotation.x = Math.PI/2;
+
+        scene.add(box);
+        scene.add(plane);
+            
+        renderer.render(scene, camera);
+
+        return scene;
+    }
+
+
+
+    useEffect(() => {
+
+
+        var scene = init();
+
+        scene.visible = false
+
+        console.log(scene)
+
+        // scene.visible = false
+
+        
+    
+        
+
+        }, []);
+
+    return (
+        <div>
+            <div ref={sceneRef} />
+            <h3>Three goes here!!!!</h3>
+        </div>
+        
+    )
+
+}
+
+
+export default Three;
--- evening-talk-app/src/pages/HomePage.jsx
+++ evening-talk-app/src/pages/HomePage.jsx
@@ -1,7 +1,10 @@
+import Three from "../components/Three";
+
 const HomePage = () => {
   return (
     <>
       <h1>Evening Talk App</h1>
+      <Three></Three>
       <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestias
          architecto ab perferendis nesciunt, aliquam assumenda dolores nostrum, 
          ad accusantium placeat magnam nobis voluptatem hic omnis repellat ex? 
